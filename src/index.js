8665a62c4b7d15a4c371e23f7819c85075b9007f
Olga Belikova <lakshmy712@mail.ru>
2019-10-04 16:50:26 +0300

    feat: add js-edu task

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+module.exports = function getTimeForEducation(focus, knowsProgramming = true, config) {
+  const learnBasics = 500;
+  const learnToMaster = 800;
+  const learnFromScratch = learnBasics + learnToMaster;
+  let weeks = 0;
+
+  if(knowsProgramming) {
+    weeks = Math.ceil(learnToMaster / config[focus]);
+  }
+  else
+    weeks = Math.ceil(learnFromScratch / config[focus]);
+  return weeks;
+};
